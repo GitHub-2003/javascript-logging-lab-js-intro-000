209b31ddcd2ad169abcbfbac7d5d98549536b4f1
MJU <marie.ju2012@outlook.com>
2020-03-01 01:05:22 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -20,7 +20,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.log()', done => {
+  it('calls console.log("Need to log something in this function")', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     jsdom.env(html, [src], {
